cba3b95504bcadc994fe1a95dad46859c38ec6d5
Leon <leon.id@qq.com>
2020-02-17 18:14:52 +0800

    配置部署本地可视化站点；

--- WinFormMetricsDotNetDemo/Program.cs
+++ WinFormMetricsDotNetDemo/Program.cs
@@ -1,21 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
+using Metrics;
 
 namespace WinFormMetricsDotNetDemo
 {
-    static class Program
+    internal static class Program
     {
+        private const string HttpUriPrefix = "http://localhost:9090/";
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
+            ConfigMetrics();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static void ConfigMetrics()
+        {
+            Metric.Config
+                .WithHttpEndpoint(HttpUriPrefix);
         }
     }
 }
